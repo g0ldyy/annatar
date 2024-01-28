5407f4737c7f6a682e4531c27232e15fad49b852
Eru Ilúvatar <eruiluvatar@pnbx.xyz>
2024-01-28 00:35:00 -0600

    cleanup

--- stremio_jackett/main.py
+++ stremio_jackett/main.py
@@ -1,4 +1,3 @@
-import json
 from typing import Any, Optional
 
 from fastapi import FastAPI
--- stremio_jackett/stremio.py
+++ stremio_jackett/stremio.py
@@ -15,18 +15,6 @@ class StreamResponse(BaseModel):
     error: str | None = None
 
 
-# export async function getName(id, type) {
-# 	if (typeof id !== "string") {
-# 		return id;
-# 	}
-
-# 	const res = await fetch(`https://v3-cinemeta.strem.io/meta/${type}/${id}.json`);
-# 	const name = (await res.json()).meta.name;
-
-# 	return name;
-# }
-
-
 class MediaInfo(BaseModel):
     id: str
     type: str
@@ -63,28 +51,3 @@ async def get_media_info(id: str, type: str) -> MediaInfo | None:
             meta = response_json["meta"]
             media_info = MediaInfo(**meta)
             return media_info
-
-
-# type Meta struct {
-# 	ID   string `json:"id"`
-# 	Type string `json:"type"`
-# 	Name string `json:"name"`
-
-# 	// Optional
-# 	Genres      []string `json:"genres,omitempty"`
-# 	Director    []string `json:"director,omitempty"`
-# 	Cast        []string `json:"cast,omitempty"`
-# 	Poster      string   `json:"poster,omitempty"`
-# 	PosterShape string   `json:"posterShape,omitempty"`
-# 	Background  string   `json:"background,omitempty"`
-# 	Logo        string   `json:"logo,omitempty"`
-# 	Description string   `json:"description,omitempty"`
-# 	ReleaseInfo string   `json:"releaseInfo,omitempty"` // A.k.a. *year*. E.g. "2000" for movies and "2000-2014" or "2000-" for TV shows
-# 	IMDbRating  string   `json:"imdbRating,omitempty"`
-# 	Released    string   `json:"released,omitempty"` // ISO 8601, e.g. "2010-12-06T05:00:00.000Z"
-# 	Runtime     string   `json:"runtime,omitempty"`
-# 	Language    string   `json:"language,omitempty"`
-# 	Country     string   `json:"country,omitempty"`
-# 	Awards      string   `json:"awards,omitempty"`
-# 	Website     string   `json:"website,omitempty"`
-# }
